8389a1ec09eef23cf3178ebd18b0805ffd5e41ab
ANDRES KWAN <andreskwan@gmail.com>
2015-01-17 13:06:20 -0500

    Flickr - working on iPhone

--- ios7-v11-Tableview/FlickrPhotosTVC.m
+++ ios7-v11-Tableview/FlickrPhotosTVC.m
@@ -8,6 +8,7 @@
 
 #import "FlickrPhotosTVC.h"
 #import "FlickrFetcher.h"
+#import "ImageViewController.h"
 
 @interface FlickrPhotosTVC ()
 
@@ -115,14 +116,32 @@
 }
 */
 
-/*
+
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)prepareImageViewController:(ImageViewController *)ivc
+                    toDisplayPhoto:(NSDictionary *)photo
+{
+    ivc.imageURL = [FlickrFetcher URLforPhoto:photo
+                                       format:FlickrPhotoFormatOriginal];
+    ivc.title = [photo valueForKey:FLICKR_PHOTO_TITLE];
 }
-*/
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue
+                 sender:(id)sender {
+    //     Get the new view controller using [segue destinationViewController].
+    //     Pass the selected object to the new view controller.
+    if ([sender isKindOfClass:[UITableViewCell class]]) {
+        NSIndexPath * indexPath = [self.tableView indexPathForCell:sender];
+        if (indexPath) {
+            if ([segue.destinationViewController isKindOfClass:[ImageViewController class]]) {
+                [self prepareImageViewController:segue.destinationViewController
+                                  toDisplayPhoto:self.photos[indexPath.row]];
+            }
+        }
+    }
+}
+
 
 @end
